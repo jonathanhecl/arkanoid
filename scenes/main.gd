520b63e39f86387a3b580f58311f9592d946e4af
Jonathan Hecl <jonathanhecl@gmail.com>
2023-10-01 22:22:46 -0300

    Fix

--- scenes/main.gd
+++ scenes/main.gd
@@ -10,6 +10,10 @@ var speed_ball = 70
 const MAX_W = 800
 const MAX_H = 600
 
+func _ready():
+	randomize()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 func _process(delta):
 	var total_balls = balls.get_child_count()
 	if total_balls == 0:
@@ -25,7 +29,8 @@ func ball_bounce():
 		if !b.bouncing:
 			b.linear_velocity = Vector2(0,0)
 			b.bouncing = true
-			b.apply_central_impulse(Vector2.UP * speed_ball)
+			b.linear_velocity = Vector2(randf_range(-100,100), 0)
+			b.apply_impulse(Vector2.UP * speed_ball * 15)
 
 func ball_over_platform():
 	for b in balls.get_children():
